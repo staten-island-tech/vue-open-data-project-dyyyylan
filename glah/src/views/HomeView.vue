edc6af791c201a00c6e230bab341671c01f62469
dyyyylan <jiangdylan00@gmail.com>
2025-03-04 12:31:03 -0500

    Update HomeView.vue

--- glah/src/views/HomeView.vue
+++ glah/src/views/HomeView.vue
@@ -1,9 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref, onMounted } from 'vue';
+const glah = ref('')
+async function hi() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/25tsh-nujf.json')
+  let data = await res.json();
+  console.log(data);
+}
+onMounted(()=> {
+  hi();
+})
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+
+</style>
