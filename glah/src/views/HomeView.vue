5a784da5261b5f4be102b665922c5a77484b7efe
dyyyylan <jiangdylan00@gmail.com>
2025-03-10 20:26:55 -0400

    Update HomeView.vue

--- glah/src/views/HomeView.vue
+++ glah/src/views/HomeView.vue
@@ -1,19 +1,20 @@
 <template>
   <div>
 <h1 v-for="(item, index) in info" :key="index">
-  {{ item }}
+  {{ item.nm }}
 </h1>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
-const info = ref('')
+const info = ref([])
 async function getinfo() {
   try {
       let res = await fetch('https://data.cityofnewyork.us/resource/25th-nujf.json')
   let data = await res.json();
-  console.log(data.data.brth_yr);
+  console.log(data);
+  info.value = data;
   }
   catch(error) {
     console.error("there is an error", error); 
